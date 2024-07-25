d8655f245023a75180557b16a86dd35eea33126c
richardshkim <65058497+richardshkimm@users.noreply.github.com>
2024-07-25 16:35:45 -0400

    FastAPI backend set up

---
+++ backend/app/routers/items.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from .. import crud, models, schemas
+from ..database import get_db
+
+router = APIRouter()
+
+@router.post("/items/", response_model=schemas.Item)
+def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    return crud.create_item(db=db, item=item)
+
+@router.get("/items/{item_id}", response_model=schemas.Item)
+def read_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = crud.get_item(db, item_id=item_id)
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return db_item
